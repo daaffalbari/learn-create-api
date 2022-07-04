a723f2ba33a7ce138be9943a7330c34179e6470a
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2022-07-04 13:54:38 +0700

    Routing request

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,11 +4,39 @@ const http = require('http');
 const requestListener = (request, response) => {
 	response.setHeader('Content-Type', 'text/html');
 	response.statusCode = 200;
-	response.end('<h1>Hello World</h1>');
+
+	const { method, url } = request;
+	if (url === '/') {
+		if (method === 'GET') {
+			response.end('<h1>Hello, ini adalah homepage</h1>');
+		} else {
+			response.end(`<h1>Halaman ini tidak bisa diakses dengan ${method} request</h1>`);
+		}
+	} else if (url === '/about') {
+		if (method === 'GET') {
+			response.end(`<h1>Halaman ini adalah halaman about</h1>`);
+		} else if (method === 'POST') {
+			let body = [];
+
+			request.on('data', (chunk) => {
+				body.push(chunk);
+			});
+
+			request.on('end', () => {
+				body = buffer.concat(body).toString();
+				const { name } = JSON.parse(body);
+				response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+			});
+		} else {
+			response.end('<h1>Halaman tidak ditemukan</h1>');
+		}
+	} else {
+		response.end('<h1>Halaman Tidak ditemukan</h1>');
+	}
 };
 
 const server = http.createServer(requestListener);
-const port = 3000;
+const port = 5000;
 const host = 'localhost';
 
 server.listen(port, host, () => {
